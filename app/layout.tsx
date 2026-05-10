ddf7679175ca91c0e77e185e7979e00dda0ac655
Takumi Takahashi <takumitakahashi@TakuminoMacBook-Pro.local>
2026-05-10 18:27:03 +0900

    Add AdSense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2913908713051662"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
